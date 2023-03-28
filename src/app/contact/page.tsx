0998f9d643110bdaae55989c7d543dda108d04f0
vhlongon <vhlongon@gmail.com>
2023-03-28 23:07:57 +0200

    add example of a client component and a button to navigate using client next router

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,22 @@
+'use client';
+
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const ContactPage = () => {
+  const router = useRouter();
+
   return (
     <>
       <div>ContactPage</div>
       <Link href="/">home</Link>
+      <Link href="/blog/123">blog 123</Link>
+      <div>
+        <button onClick={() => router.push('/')}>
+          got to home button with push
+        </button>
+      </div>
     </>
   );
 };
