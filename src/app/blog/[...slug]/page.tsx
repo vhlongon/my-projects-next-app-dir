326c40a37d6b6465f8615c169134aac1103d8b78
vhlongon <vhlongon@gmail.com>
2023-04-01 22:20:48 +0200

    pre generate pages with generateStaticParams

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -8,6 +8,10 @@ export const generateMetadata = async (metadata: BlogPageProps) => ({
   title: `Page: ${metadata.params.slug}`,
 });
 
+export const generateStaticParams = async () => {
+  return [{ slug: ['test'] }, { slug: ['test2'] }, { slug: ['test3'] }];
+};
+
 const BlogPage = ({ params }: BlogPageProps) => {
   return <div>BlogPage: {params.slug}</div>;
 };
